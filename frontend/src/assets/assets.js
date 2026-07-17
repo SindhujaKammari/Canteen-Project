da3c92d7893c0f5374579798b4e3def6ba7e3d7b
SindhujaKammari <sindhukammari0@gmail.com>
2026-07-17 14:44:00 +0530

    Fix remaining import casing in App.jsx

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -50,7 +50,7 @@ import remove_icon_red from './remove_icon_red.png'
 import app_store from './app_store.png'
 import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
-import facebook_icon from './facebook_icon.png'
+
 import twitter_icon from './twitter_icon.png'
 import cross_icon from './cross_icon.png'
 import selector_icon from './selector_icon.png'
@@ -72,7 +72,6 @@ export const assets = {
     app_store,
     play_store,
     linkedin_icon,
-    facebook_icon,
     twitter_icon,
     cross_icon,
     selector_icon,
